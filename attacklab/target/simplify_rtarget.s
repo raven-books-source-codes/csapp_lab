5b6d9fbe16635b704b513bf8af28c3c8dbda6744
ravenxrz <zhang.xingrui@foxmail.com>
2020-07-20 20:49:00 +0800

    attacklab phase4解决

---
+++ attacklab/target/simplify_rtarget.s
@@ -0,0 +1,16 @@
+
+0000000000401994 <start_farm>:
+  401994:	b8 01 00 00 00       	mov    $0x1,%eax
+  401999:	c3                   	retq   
+
+
+0000000000401ab2 <end_farm>:
+  401ab2:	b8 01 00 00 00       	mov    $0x1,%eax
+  401ab7:	c3                   	retq   
+  401ab8:	90                   	nop
+  401ab9:	90                   	nop
+  401aba:	90                   	nop
+  401abb:	90                   	nop
+  401abc:	90                   	nop
+  401abd:	90                   	nop
+  401abe:	90                   	nop
